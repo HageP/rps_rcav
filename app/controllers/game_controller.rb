9b0b19aaa19ea4ebae101d54c353f68ab8847443
Pierre Hage <phage@chicagobooth.edu>
2016-10-20 01:55:11 -0500

    Working Copy

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,10 +1,21 @@
 class GameController < ApplicationController
 
+
+
 def user_plays_rock
-moves = ["rock", "paper" , "scissors"]
- @computer_moves = moves.sample
+  moves = ["rock", "paper" , "scissors"]
+  @computer_moves = moves.sample
+
 if @computer_moves == "rock"
 @result= "you tied"
+end
+
+if @computer_moves == "paper"
+@result= "you lose"
+end
+
+if @computer_moves == "scissors"
+@result= "you win"
 end
 
 render("game/play_rock.html.erb")
@@ -13,11 +24,40 @@ end
 
 def user_plays_paper
 
+  moves = ["rock", "paper" , "scissors"]
+  @computer_moves = moves.sample
+
+  if @computer_moves == "rock"
+  @result= "you win"
+  end
+
+  if @computer_moves == "paper"
+  @result= "you tied"
+  end
+
+  if @computer_moves == "scissors"
+  @result= "you lose"
+  end
 render("game/play_paper.html.erb")
 
 end
 
 def user_plays_scissors
+
+  moves = ["rock", "paper" , "scissors"]
+  @computer_moves = moves.sample
+
+  if @computer_moves == "rock"
+  @result= "you lose"
+  end
+
+  if @computer_moves == "paper"
+  @result= "you win"
+  end
+
+  if @computer_moves == "scissors"
+  @result= "you tied"
+  end
 
 render("game/play_scissors.html.erb")
 
